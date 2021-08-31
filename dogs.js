0da815f46481cc2449b51587175603b42fc66d09
shaystevens <stesh969@student.otago.ac.nz>
2021-09-01 10:15:21 +1200

    Assignment 1 Added the dogs and images and made it possible to see a description of the dog by clicking header

--- dogs.js
+++ dogs.js
@@ -1,19 +1,45 @@
-var dogs = (function(){
+let dogs = (function(){
     "use strict";
-    var pub = {};
+    let pub = {};
 
-    function showDogs() {
-        var keys;
-        fetch('animals.json').then(function(response){
-            return response.json();
-        }).then(function (obj){
-            keys = Object.keys(obj);
-            console.log(keys[0]);
-        });
+    function rotate(){
+        let card, index;
+        let dog_name = $(this).text();
+        if(dog_name === "Doge"){
+            index = 0;
+        }
+
+        if(dog_name === "Akira"){
+            index = 1;
+        }
+
+        if(dog_name === "Ronnie"){
+            index = 2;
+        }
+
+        if(dog_name === "Daisy"){
+            index = 3;
+        }
+
+        if(dog_name === "Ava"){
+            index = 4;
+        }
+
+        if(dog_name === "Snoopy"){
+            index = 5;
+        }
+        card = $(".card-inner")[index];
+        card.classList.toggle('is-flipped');
     }
 
     pub.setup = function(){
-        showDogs();
+        let dogArray = $("#dogs").find("h4");
+        let i;
+        for(i = 0; i < dogArray.length; i++){
+            dogArray[i].style.cursor = "pointer";
+            dogArray[i].addEventListener("click", rotate);
+        }
+
     };
 
     return pub;
