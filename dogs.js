7502854917f96e9d6592bf4c9f80325d12138e91
shaystevens <stesh969@student.otago.ac.nz>
2021-09-01 12:29:16 +1200

    Edited so now clicking the image flips the card instead of title

--- dogs.js
+++ dogs.js
@@ -3,41 +3,24 @@ let dogs = (function(){
     let pub = {};
 
     function rotate(){
-        let card, index;
-        let dog_name = $(this).text();
-        if(dog_name === "Doge"){
-            index = 0;
+        let card;
+        if($(this).hasClass('card-back')){
+            card = $(this).parent()[0];
+        }else{
+            card = $(this).parent().parent()[0];
         }
-
-        if(dog_name === "Akira"){
-            index = 1;
-        }
-
-        if(dog_name === "Ronnie"){
-            index = 2;
-        }
-
-        if(dog_name === "Daisy"){
-            index = 3;
-        }
-
-        if(dog_name === "Ava"){
-            index = 4;
-        }
-
-        if(dog_name === "Snoopy"){
-            index = 5;
-        }
-        card = $(".card-inner")[index];
         card.classList.toggle('is-flipped');
     }
 
     pub.setup = function(){
-        let dogArray = $("#dogs").find("h4");
+        let dogArray = $("#dogs").find("img");
+        let array = $(".card-back");
         let i;
         for(i = 0; i < dogArray.length; i++){
             dogArray[i].style.cursor = "pointer";
+            array[i].style.cursor = "pointer";
             dogArray[i].addEventListener("click", rotate);
+            array[i].addEventListener("click", rotate);
         }
 
     };
