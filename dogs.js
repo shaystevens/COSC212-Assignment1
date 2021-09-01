b89001a8f40368e74d32f961d85fc77314494226
shaystevens <stesh969@student.otago.ac.nz>
2021-09-01 15:18:44 +1200

    Added the dog description to each dog

--- dogs.js
+++ dogs.js
@@ -12,7 +12,52 @@ let dogs = (function(){
         card.classList.toggle('is-flipped');
     }
 
+    function dogDescription(data){
+        let dogArray = data.animals.dogs;
+        let x, i, newTag, newText;
+        for(x=0; x < dogArray.length; x++){
+            let tag = $('.card-back')[x];
+            for(i=0; i < dogArray.length-1; i++){
+                newTag = document.createElement("p");
+
+                if(i === 0){
+                    newText = document.createTextNode("Name: " + dogArray[x].dogName);
+                    newTag.appendChild(newText);
+                    tag.append(newTag);
+                }
+
+                if(i === 1){
+                    newText = document.createTextNode("Breed: " + dogArray[x].dogType);
+                    newTag.appendChild(newText);
+                    tag.append(newTag);
+                }
+
+                if(i === 2){
+                    newText = document.createTextNode("Size: " + dogArray[x].dogSize);
+                    newTag.appendChild(newText);
+                    tag.append(newTag);
+                }
+
+                if(i === 3){
+                    newText = document.createTextNode("About me: ");
+                    let breakTag = document.createElement("p");
+                    let newLine = document.createTextNode(dogArray[x].description);
+                    breakTag.appendChild(newLine);
+                    newTag.appendChild(newText);
+                    tag.append(newTag);
+                    tag.append(breakTag);
+                }
+                if(i === 4){
+                    newText = document.createTextNode("Price: $" + dogArray[x].pricePerHour + " (per hour)");
+                    newTag.appendChild(newText);
+                    tag.append(newTag);
+                }
+            }
+       }
+    }
+
     pub.setup = function(){
+        let jsonFile = "animals.json";
         let dogArray = $("#dogs").find("img");
         let array = $(".card-back");
         let i;
@@ -22,6 +67,15 @@ let dogs = (function(){
             dogArray[i].addEventListener("click", rotate);
             array[i].addEventListener("click", rotate);
         }
+
+        $.ajax({
+            type: "GET",
+            url: jsonFile,
+            cache: false,
+            success: function(data) {
+                dogDescription(data);
+            }
+        });
 
     };
 
